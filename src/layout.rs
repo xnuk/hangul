f393ec763dda58b740a150e2a303f2d19f3e9aa3
Xnuk Shuman <admin@xnu.kr>
2021-01-17 04:44:43 +0900

    대충 암캐나

--- src/layout.rs
+++ src/layout.rs
@@ -1,6 +1,112 @@
-struct PrintableASCII;
+pub trait Key {
+	fn has_shift(&self) -> bool { false }
+	fn has_alt(&self) -> bool { false }
+	fn has_ctrl(&self) -> bool { false }
+}
 
+#[derive(Debug)]
+pub enum WritingEvent {
+	Commit(String, Option<String>),
+	Display(String),
+	Cancel,
+	None,
+}
 
-trait KeyInputHandler {
-	fn keydown(&self, )
+pub trait KeyInputHandler<K: Key> {
+	type Candidates: IntoIterator<Item = String>;
+
+	fn keydown(&mut self, _key: &K) -> WritingEvent { WritingEvent::None }
+	fn keyup  (&mut self, _key: &K) -> WritingEvent { WritingEvent::None }
+
+	fn candidates(&self) -> Option<&Self::Candidates> { None }
+	fn select_candidate(&mut self, _item: &String) -> WritingEvent {
+		WritingEvent::None
+	}
+}
+
+pub struct QWERTY(pub u8);
+
+impl Key for QWERTY {
+	fn has_shift(&self) -> bool {
+		let QWERTY(code) = *self;
+		match code {
+			| u8::MIN..=31
+			| b' '
+			| b'\''
+			| b','
+			| b'-'
+			| b'.'
+			| b'/'
+			| b'0'..=b'9'
+			| b';'
+			| b'='
+			| b'[' | b']'
+			| b'\\'
+			| b'`'
+			| b'a'..=b'z'
+			=> false,
+
+			| 127..=u8::MAX
+			| b'A'..=b'Z'
+			| b'!'..=b'+'
+			| b':'
+			| b'<' | b'>'
+			| b'{' | b'}'
+			| b'?' | b'@'
+			| b'|' | b'~'
+			| b'^' | b'_'
+			=> true,
+		}
+	}
+}
+
+#[derive(Default, Debug)]
+pub struct StateTest {
+	candidates: Vec<String>,
+	compositing: String,
+}
+
+impl KeyInputHandler<QWERTY> for StateTest {
+	type Candidates = Vec<String>;
+
+	fn keydown(&mut self, key: &QWERTY) -> WritingEvent {
+		let QWERTY(code) = key;
+		self.candidates = match code {
+			b'A' => vec![
+				"Apple".to_string(),
+				"Banana".to_string(),
+				"Kiwi".to_string(),
+			],
+
+			b'a' => vec![
+				"anal sex".to_string(),
+				"oral sex".to_string(),
+				"prostate massage".to_string(),
+			],
+
+			_ => vec![]
+		};
+
+		if self.candidates.len() > 0 {
+			self.compositing += "s";
+			WritingEvent::Display(self.compositing.clone())
+		} else {
+			let cl = self.compositing.clone();
+			self.compositing = String::from(char::from(*code));
+			WritingEvent::Commit(cl, Some(self.compositing.clone()))
+		}
+	}
+
+	fn candidates(&self) -> Option<&Vec<String>> {
+		if self.candidates.len() > 0 {
+			Some(&self.candidates)
+		} else {
+			None
+		}
+	}
+
+	fn select_candidate(&mut self, item: &String) -> WritingEvent {
+		self.compositing.clear();
+		WritingEvent::Commit(item.clone(), None)
+	}
 }
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,29 @@
 use std::io;
 
-fn something(line: &str) {
-	let result = hangeul::decompose(&line);
-	println!("{:?}", result)
-}
+use prostate::layout::{Key, KeyInputHandler, QWERTY, StateTest};
+
+// fn something(line: &str) {
+// 	let result = hangeul::decompose(&line);
+// 	println!("{:?}", result)
+// }
 
 fn main() -> io::Result<()> {
-	let mut line = String::new();
-	io::stdin().read_line(&mut line)?;
-	let line = line.trim_end_matches::<&[char]>(&['\r', '\n']);
-	something(&line);
+	// let mut line = String::new();
+	// io::stdin().read_line(&mut line)?;
+	// let line = line.trim_end_matches::<&[char]>(&['\r', '\n']);
+	// something(&line);
+
+	println!("{:?}", QWERTY(b'A').has_shift());
+
+	let mut st: StateTest = Default::default();
+	println!("{:?}", &st);
+
+	println!("{:?}\t\t{:?}", st.keydown(&QWERTY(b'B')), &st);
+	println!("{:?}\t\t{:?}", st.keydown(&QWERTY(b'A')), &st);
+	println!("{:?}\t\t{:?}", st.keydown(&QWERTY(b'N')), &st);
+	println!("{:?}\t\t{:?}", st.keydown(&QWERTY(b'a')), &st);
+	println!("{:?}\t\t{:?}", st.keydown(&QWERTY(b'n')), &st);
+	println!("{:?}\t\t{:?}", st.select_candidate(&"prostate".to_string()), &st);
+
 	Ok(())
 }
